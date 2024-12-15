02924bbbdf182df2ba528ac47fdda546e4d1f6fc
Andre Landgraf <andre.timo.landgraf@gmail.com>
2024-12-15 11:46:55 -0800

    highlight when yanking text

--- init.lua
+++ init.lua
@@ -2,6 +2,10 @@
 -- Source/execute current lua file:
 vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
 
+-- Clear highlights on search when pressing <Esc> in normal mode
+--  See `:help hlsearch`
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
 -- Nvim configuration options
 -- Show current command:
 vim.opt.showcmd = true
@@ -20,6 +24,14 @@ vim.schedule(function()
   vim.opt.clipboard = 'unnamedplus'
 end)
 
--- Clear highlights on search when pressing <Esc> in normal mode
---  See `:help hlsearch`
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
